dd2340660223888198f035390dfe39ed44ac5302
wajdi <wajdi@WORKGROUP_ID012REC>
2017-02-12 19:04:37 +0100

    Class West have not been commited the last time

---
+++ mars-rover/src/main/java/com/talan/kata/West.java
@@ -0,0 +1,15 @@
+package com.talan.kata;
+
+public class West implements Direction {
+	
+	private Coordinate coordinate;	
+
+	public West(Coordinate coordinate) {
+		this.coordinate = coordinate;
+	}
+
+	public Direction turnRight() {
+		return new North(coordinate);
+	}
+
+}
